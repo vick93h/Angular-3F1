c168c90c758d448e091a7fdfb888dce2358457b4
vick93h <vittoriogerry@gmail.com>
2023-01-03 22:44:47 -0800

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { TabellaComponent } from './tabella/tabella.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabellaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
