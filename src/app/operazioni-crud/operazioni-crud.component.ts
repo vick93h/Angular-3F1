7de7b2506d42ac2028453a090e662584478c7f55
vick93h <vittoriogerry@gmail.com>
2023-01-12 22:27:27 +0100

    restyling compito

---
+++ src/app/operazioni-crud/operazioni-crud.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operazioni-crud',
+  templateUrl: './operazioni-crud.component.html',
+  styleUrls: ['./operazioni-crud.component.css']
+})
+export class OperazioniCrudComponent {
+
+}
