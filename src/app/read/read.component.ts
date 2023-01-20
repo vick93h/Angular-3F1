073c04be9904b40a773d68b3b268428d5572d40b
vick93h <vittoriogerry@gmail.com>
2023-01-20 21:29:22 +0100

    restyling compito

--- src/app/read/read.component.ts
+++ src/app/read/read.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import {RestServiceService} from '../services/rest-service.service';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-read',
   templateUrl: './read.component.html',
   styleUrls: ['./read.component.css']
 })
-export class ReadComponent {
+export class ReadComponent implements AfterViewInit {
+  constructor(private serviceRest: RestServiceService) {
 
+  }
+
+  ngAfterViewInit(): void {
+    this.serviceRest.areaCondivisa.selectRead=false;
+    console.log("read in after view init")
+    this.read();
+  }
+  read(){
+    console.log("read....");
+    this.serviceRest.getDati().subscribe(()=>{});
+  }
 }
